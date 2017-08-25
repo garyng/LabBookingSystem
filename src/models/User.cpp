56a7ae784836734ae1a55c191d5524a2f73c8fb9
GaryNg <garyng.zhongbo@gmail.com>
2017-08-26 02:29:55 +0800

    feat(model): add User

---
+++ src/models/User.cpp
@@ -0,0 +1,32 @@
+﻿#include "User.h"
+#include <json.hpp>
+
+
+User::User(const std::string& id, const std::string& name, const std::string& hashedPassword, const UserRole& role): _id(id),
+                                                                                                                     _name(name),
+                                                                                                                     _hashedPassword(hashedPassword),
+                                                                                                                     _role(role._to_string())
+{
+}
+
+void to_json(nlohmann::json& json, const User& user)
+{
+	json = nlohmann::json{
+		{"Id", user.Id()},
+		{"Name", user.Name()},
+		{"HashedPassword", user.HashedPassword()},
+		{"Role", user.Role()}
+	};
+}
+
+void from_json(const nlohmann::json& json, User& user)
+{
+	user.Id(json.at("Id").get<std::string>());
+
+	user.Name(json.at("Name").get<std::string>());
+	user.HashedPassword(json.at("HashedPassword").get<std::string>());
+	user.Role(UserRole::_from_string(
+		json.at("Role")
+		    .get<std::string>()
+		    .c_str()));
+}
---
+++ src/models/User.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include <string>
+#include <json.hpp>
+#include <iostream>
+#include <enum.h>
+
+BETTER_ENUM(UserRole, int, Adminstrator, Normal);
+
+class User
+{
+private:
+	std::string _id;
+	std::string _name;
+	std::string _hashedPassword;
+	std::string _role;
+
+public:
+
+	std::string Id() const { return _id; }
+	void Id(std::string value) { _id = value; }
+
+	std::string Name() const { return _name; }
+	void Name(std::string value) { _name = value; }
+
+	std::string HashedPassword() const { return _hashedPassword; }
+	void HashedPassword(std::string hashedPassword) { _hashedPassword = hashedPassword; }
+
+	std::string Role() const { return _role; }
+	void Role(UserRole role) { _role = role._to_string(); }
+
+	User() = default;
+	User(const std::string& id, const std::string& name, const std::string& hashedPassword, const UserRole& role);
+};
+
+void to_json(nlohmann::json& json, const User& user);
+void from_json(const nlohmann::json& json, User& user);
