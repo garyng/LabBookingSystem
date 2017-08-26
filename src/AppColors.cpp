48f929203f12465ce587010e3df2d42a3e8af6f8
GaryNg <garyng.zhongbo@gmail.com>
2017-08-27 00:29:49 +0800

    feat: add AppColors (taken from Google Material Color)

---
+++ src/AppColors.cpp
@@ -0,0 +1,21 @@
+﻿#include "AppColors.h"
+AppColors AppColors::DefaultWhite(ImVec4(0.90f, 0.90f, 0.90f, 1.00f));
+AppColors AppColors::Red500(ImVec4(0.957f, 0.263f, 0.212f, 1.0f));
+AppColors AppColors::Pink500(ImVec4(0.914f, 0.118f, 0.388f, 1.0f));
+AppColors AppColors::Purple500(ImVec4(0.612f, 0.153f, 0.69f, 1.0f));
+AppColors AppColors::DeepPurple500(ImVec4(0.404f, 0.227f, 0.718f, 1.0f));
+AppColors AppColors::Indigo500(ImVec4(0.247f, 0.318f, 0.71f, 1.0f));
+AppColors AppColors::Blue500(ImVec4(0.129f, 0.588f, 0.953f, 1.0f));
+AppColors AppColors::LightBlue500(ImVec4(0.012f, 0.663f, 0.957f, 1.0f));
+AppColors AppColors::Cyan500(ImVec4(0.0f, 0.737f, 0.831f, 1.0f));
+AppColors AppColors::Teal500(ImVec4(0.0f, 0.588f, 0.533f, 1.0f));
+AppColors AppColors::Green500(ImVec4(0.298f, 0.686f, 0.314f, 1.0f));
+AppColors AppColors::LightGreen500(ImVec4(0.545f, 0.765f, 0.29f, 1.0f));
+AppColors AppColors::Lime500(ImVec4(0.804f, 0.863f, 0.224f, 1.0f));
+AppColors AppColors::Yellow500(ImVec4(1.0f, 0.922f, 0.231f, 1.0f));
+AppColors AppColors::Amber500(ImVec4(1.0f, 0.757f, 0.027f, 1.0f));
+AppColors AppColors::Orange500(ImVec4(1.0f, 0.596f, 0.0f, 1.0f));
+AppColors AppColors::DeepOrange500(ImVec4(1.0f, 0.341f, 0.133f, 1.0f));
+AppColors AppColors::Brown500(ImVec4(0.475f, 0.333f, 0.282f, 1.0f));
+AppColors AppColors::Grey500(ImVec4(0.62f, 0.62f, 0.62f, 1.0f));
+AppColors AppColors::BlueGrey500(ImVec4(0.376f, 0.49f, 0.545f, 1.0f));
---
+++ src/AppColors.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#pragma once
+#include "imgui.h"
+
+class AppColors
+{
+private:
+	AppColors(ImVec4 color) : Color(color)
+	{
+	}
+
+public:
+	static AppColors DefaultWhite;
+	static AppColors Red500;
+	static AppColors Pink500;
+	static AppColors Purple500;
+	static AppColors DeepPurple500;
+	static AppColors Indigo500;
+	static AppColors Blue500;
+	static AppColors LightBlue500;
+	static AppColors Cyan500;
+	static AppColors Teal500;
+	static AppColors Green500;
+	static AppColors LightGreen500;
+	static AppColors Lime500;
+	static AppColors Yellow500;
+	static AppColors Amber500;
+	static AppColors Orange500;
+	static AppColors DeepOrange500;
+	static AppColors Brown500;
+	static AppColors Grey500;
+	static AppColors BlueGrey500;
+
+	const ImVec4 Color;
+	operator ImVec4() const { return Color; }
+};
