507018725b7ebeea72f81f36439e8324f7a23416
GaryNg <garyng.zhongbo@gmail.com>
2017-08-28 00:01:56 +0800

    feat: add Logger to ViewModelBase

--- src/ViewModelBase.h
+++ src/ViewModelBase.h
@@ -1,11 +1,14 @@
 ﻿#pragma once
 #include <memory>
 #include "services/NavigationService.h"
+#include "logger/Logger.h"
+
 
 class ViewModelBase
 {
 protected:
 	std::shared_ptr<NavigationService> _navigation;
+	std::shared_ptr<ILogger> Log = Logger::Instance();
 
 public:
 	ViewModelBase(const std::shared_ptr<NavigationService>& navigation);
