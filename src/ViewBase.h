b9ad5bfabd9edc1ed8b9099e680d99c7111999ef
GaryNg <garyng.zhongbo@gmail.com>
2017-08-28 01:22:02 +0800

    feat(view): add help method to clear buffer

--- src/ViewBase.h
+++ src/ViewBase.h
@@ -2,12 +2,19 @@
 #include "ViewModelBase.h"
 
 template <class TViewModel,
-	std::enable_if_t<std::is_base_of<ViewModelBase, TViewModel>::value>* = nullptr>
-	class ViewBase
+          std::enable_if_t<std::is_base_of<ViewModelBase, TViewModel>::value>* = nullptr>
+class ViewBase
 	: public IView
 {
 protected:
 	std::shared_ptr<TViewModel> _viewModel;
+
+	template <class Arr, class Val>
+	void ClearBuffer(Arr* buffer, const Val& value)
+	{
+		std::fill_n(buffer, static_cast<int>(sizeof(buffer)) / (sizeof(*buffer)), value);
+	}
+
 public:
 	explicit ViewBase(const std::shared_ptr<TViewModel>& viewModel)
 		: _viewModel(viewModel)
