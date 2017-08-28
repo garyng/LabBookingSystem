9a7359c26f9c013f869c323a2df79387eec158d9
GaryNg <garyng.zhongbo@gmail.com>
2017-08-28 14:35:34 +0800

    refactor(navigation): use enable_if_t instead of enable_if

--- src/services/NavigationService.h
+++ src/services/NavigationService.h
@@ -15,29 +15,31 @@ private:
 	std::map<std::type_index, std::pair<std::shared_ptr<ViewModelBase>, std::shared_ptr<IView>>> _viewModelViewPair;
 
 public:
-	template <class TViewModel, class TView>
-	typename std::enable_if<std::is_base_of<ViewModelBase, TViewModel>::value &&
-	                        std::is_base_of<IView, TView>::value,
-	                        void>::type
-	Register(const std::shared_ptr<ViewModelBase>& viewModel, const std::shared_ptr<IView>& view)
+	template <class TViewModel, class TView,
+	          std::enable_if_t<
+		          std::is_base_of<ViewModelBase, TViewModel>::value &&
+		          std::is_base_of<IView, TView>::value>* = nullptr>
+	void Register(const std::shared_ptr<ViewModelBase>& viewModel, const std::shared_ptr<IView>& view)
 	{
 		std::type_index key = typeid(TViewModel);
 		_viewModelViewPair.insert({key, {viewModel, view}});
 	}
 
 
-	template <class TViewModel>
-	typename std::enable_if<std::is_base_of<ViewModelBase, TViewModel>::value, void>::type
-	NavigateTo()
+	template <class TViewModel,
+	          std::enable_if_t<
+		          std::is_base_of<ViewModelBase, TViewModel>::value>* = nullptr>
+	void NavigateTo()
 	{
 		NavigateTo<TViewModel>([](std::shared_ptr<ViewModelBase> vm)
 			{
 			});
 	}
 
-	template <class TViewModel, class CallBack>
-	typename std::enable_if<std::is_base_of<ViewModelBase, TViewModel>::value, void>::type
-	NavigateTo(CallBack&& callBack)
+	template <class TViewModel, class CallBack,
+	          std::enable_if_t<
+		          std::is_base_of<ViewModelBase, TViewModel>::value>* = nullptr>
+	void NavigateTo(CallBack&& callBack)
 	{
 		std::type_index key = typeid(TViewModel);
 		std::map<std::type_index, std::pair<std::shared_ptr<ViewModelBase>, std::shared_ptr<IView>>>::iterator result = _viewModelViewPair.find(key);
