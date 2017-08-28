fb478cefe64d25b0f04da8577850f2d875eb9d11
GaryNg <garyng.zhongbo@gmail.com>
2017-08-28 14:49:09 +0800

    refactor(navigation): extract pair to ViewViewModel pair class

--- src/services/NavigationService.h
+++ src/services/NavigationService.h
@@ -3,17 +3,37 @@
 #include <typeindex>
 #include <memory>
 #include <functional>
+#include <stack>
 #include "../IView.h"
 
 class ViewModelBase;
+
+class ViewViewModelPair
+{
+private:
+	std::shared_ptr<ViewModelBase> _viewModelBase;
+
+	std::shared_ptr<IView> _view;
+
+public:
+	std::shared_ptr<ViewModelBase> ViewModelBase() const { return _viewModelBase; }
+
+	std::shared_ptr<IView> View() const { return _view; }
+
+	ViewViewModelPair(const std::shared_ptr<::ViewModelBase>& viewModelBase, const std::shared_ptr<IView>& view)
+		: _viewModelBase(viewModelBase),
+		  _view(view)
+	{
+	}
+};
 
 class NavigationService
 {
 private:
 
 	std::shared_ptr<IView> _currentView;
-	std::map<std::type_index, std::pair<std::shared_ptr<ViewModelBase>, std::shared_ptr<IView>>> _viewModelViewPair;
-
+	std::map<std::type_index, ViewViewModelPair> _viewModelViewPair;
+	std::stack<std::pair<std::shared_ptr<ViewModelBase>, std::shared_ptr<IView>>> _navigationHistory;
 public:
 	template <class TViewModel, class TView,
 	          std::enable_if_t<
@@ -42,15 +62,18 @@ public:
 	void NavigateTo(CallBack&& callBack)
 	{
 		std::type_index key = typeid(TViewModel);
-		std::map<std::type_index, std::pair<std::shared_ptr<ViewModelBase>, std::shared_ptr<IView>>>::iterator result = _viewModelViewPair.find(key);
-		std::shared_ptr<ViewModelBase> viewModel = result->second.first;
+
+		std::map<std::type_index, ViewViewModelPair>::iterator result = _viewModelViewPair.find(key);
+		std::shared_ptr<ViewModelBase> viewModel = result->second.ViewModelBase();
+
 		std::shared_ptr<TViewModel> castedViewModel = std::static_pointer_cast<TViewModel>(viewModel);
 		callBack(castedViewModel);
 		if (result != _viewModelViewPair.end())
 		{
-			_currentView = result->second.second;
+			_currentView = result->second.View();
 		}
 	}
+
 
 	void RenderCurrent() const
 	{
@@ -61,7 +84,7 @@ public:
 	{
 		for (auto& p :_viewModelViewPair)
 		{
-			p.second.second->Render();
+			p.second.View()->Render();
 		}
 	}
 };
