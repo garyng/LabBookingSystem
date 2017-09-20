013593028d71bb57bb9280b16080ed5845e1c456
GaryNg <garyng.zhongbo@gmail.com>
2017-09-20 21:19:26 +0800

    refactor(main): simplify views and view models registration

--- src/Main.cpp
+++ src/Main.cpp
@@ -25,13 +25,46 @@
 
 using namespace std;
 
-template <class TView, class TViewModel,
-          enable_if_t<is_base_of<IView, TView>::value && is_base_of<ViewModelBase, TViewModel>::value>* = nullptr>
-void MakeViewsAndViewModels(const shared_ptr<NavigationService>& navigation)
+template <class TView, class TViewModel, class... TViewModelArgs,
+          enable_if_t<
+	          is_base_of<ViewModelBase, TViewModel>::value &&
+	          is_base_of<IView, TView>::value
+          >* = nullptr>
+void RegisterMVVM(const shared_ptr<NavigationService>& navigation, TViewModelArgs&&... viewModelArgs)
 {
-	shared_ptr<TViewModel> viewModel = make_shared<TViewModel>(navigation);
+	shared_ptr<TViewModel> viewModel = make_shared<TViewModel>(navigation, forward<TViewModelArgs>(viewModelArgs)...);
 	shared_ptr<TView> view = make_shared<TView>(viewModel);
 	navigation->Register<TViewModel, TView>(viewModel, view);
+}
+
+template <class TView, class... TViewArgs,
+          enable_if_t<is_base_of<IView, TView>::value>* = nullptr>
+void RegisterDebug(const shared_ptr<ViewsRenderer>& renderer, TViewArgs&&... viewArgs)
+{
+	shared_ptr<TView> view = make_shared<TView>(forward<TViewArgs>(viewArgs)...);
+	renderer->RegisterToDebugMenu<TView>(view);
+}
+
+void AutomateGui(const shared_ptr<NavigationService>& navigation)
+{
+	navigation->GoTo<LoginViewModel>([](shared_ptr<LoginViewModel> vm)
+	{
+		vm->LoginCommand("1", "1");
+	});
+	navigation->GoTo<AdminViewModel>([](shared_ptr<AdminViewModel> vm)
+                                 {
+	                                 vm->UserName("1");
+                                 }, true);
+	navigation->GoTo<RequestViewModel>([](shared_ptr<RequestViewModel> vm)
+                                   {
+	                                   vm->UserName("1");
+	                                   vm->LoadUserRequestCommand();
+                                   }, true);
+	navigation->GoTo<SelectLabViewModel>([](shared_ptr<SelectLabViewModel> vm)
+                                     {
+	                                     vm->UserName("1");
+	                                     vm->LoadAllLabsCommand();
+                                     }, true);
 }
 
 int main(int argc, char* argv[])
@@ -45,47 +78,29 @@ int main(int argc, char* argv[])
 	shared_ptr<RequestStorage> requestStorage = make_shared<RequestStorage>();
 	requestStorage->Load();
 
-	shared_ptr<LoginViewModel> loginViewModel = make_shared<LoginViewModel>(navigation, userStorage);
-	shared_ptr<LoginView> loginView = make_shared<LoginView>(loginViewModel);
-	navigation->Register<LoginViewModel, LoginView>(loginViewModel, loginView);
-
-
-	MakeViewsAndViewModels<UserView, UserViewModel>(navigation);
-	MakeViewsAndViewModels<AdminView, AdminViewModel>(navigation);
-
-	shared_ptr<RequestViewModel> requestViewModel = make_shared<RequestViewModel>(navigation, userStorage, requestStorage);
-	shared_ptr<RequestView> requestView = make_shared<RequestView>(requestViewModel);
-	navigation->Register<RequestViewModel, RequestView>(requestViewModel, requestView);
-
-	shared_ptr<SelectLabViewModel> selectLabViewModel = make_shared<SelectLabViewModel>(navigation, labStorage);
-	shared_ptr<SelectLabView> selectLabView = make_shared<SelectLabView>(selectLabViewModel);
-	navigation->Register<SelectLabViewModel, SelectLabView>(selectLabViewModel, selectLabView);
+	RegisterMVVM<LoginView, LoginViewModel>(navigation, userStorage);
+	RegisterMVVM<UserView, UserViewModel>(navigation);
+	RegisterMVVM<AdminView, AdminViewModel>(navigation);
+	RegisterMVVM<RequestView, RequestViewModel>(navigation, userStorage, requestStorage);
+	RegisterMVVM<SelectLabView, SelectLabViewModel>(navigation, labStorage);
 
 	navigation->GoTo<LoginViewModel>();
 
-
 	shared_ptr<ViewsRenderer> renderer = make_shared<ViewsRenderer>(navigation);
-	shared_ptr<AppColorsTestView> appColorsTestView = make_shared<AppColorsTestView>();
-	shared_ptr<AppFontsTestView> appFontsTestView = make_shared<AppFontsTestView>();
-	shared_ptr<ImGuiStyleEditorView> imGuiStyleEditorView = make_shared<ImGuiStyleEditorView>();
-	shared_ptr<DataStorageTestsView> dataStorageTestsView = make_shared<DataStorageTestsView>(vector<shared_ptr<IStorage>>{userStorage, requestStorage, labStorage});
-	shared_ptr<ImGuiDateChooserTestView> imGuiDateChooserTestView = make_shared<ImGuiDateChooserTestView>();
-	shared_ptr<ImGuiMetricsView> imGuiMetricsView = make_shared<ImGuiMetricsView>();
-	shared_ptr<TimeRangeCalculationTestsView> timeRangeCalculationTestsView = make_shared<TimeRangeCalculationTestsView>();
 
-	renderer->RegisterToDebugMenu<AppColorsTestView>(appColorsTestView);
-	renderer->RegisterToDebugMenu<AppFontsTestView>(appFontsTestView);
-	renderer->RegisterToDebugMenu<ImGuiStyleEditorView>(imGuiStyleEditorView);
-	renderer->RegisterToDebugMenu<DataStorageTestsView>(dataStorageTestsView);
-	renderer->RegisterToDebugMenu<ImGuiDateChooserTestView>(imGuiDateChooserTestView);
-	renderer->RegisterToDebugMenu<TimeRangeCalculationTestsView>(timeRangeCalculationTestsView);
-	renderer->RegisterToDebugMenu<ImGuiMetricsView>(imGuiMetricsView);
+	RegisterDebug<AppColorsTestView>(renderer);
+	RegisterDebug<AppFontsTestView>(renderer);
+	RegisterDebug<ImGuiStyleEditorView>(renderer);
+	RegisterDebug<DataStorageTestsView>(renderer, vector<shared_ptr<IStorage>>{userStorage, requestStorage, labStorage});
+	RegisterDebug<ImGuiDateChooserTestView>(renderer);
+	RegisterDebug<ImGuiMetricsView>(renderer);
+	RegisterDebug<TimeRangeCalculationTestsView>(renderer);
+
 
 	shared_ptr<App> app = make_shared<App>(renderer);
 
 	// automate gui
-	loginViewModel->LoginCommand("1", "1");
-
+	AutomateGui(navigation);
 
 	app->Start();
 
